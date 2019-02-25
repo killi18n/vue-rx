b25971d88e0e411501dab5d358651254fadc9d12
Dongho Choi <evals4dead@gmail.com>
2019-02-25 11:49:26 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,26 @@
     <h1 class="title">{{timesTwo$}}</h1>
     <h1 class="title">{{timesThree$}}</h1>
     <h1 class="title">{{random$}}</h1>
-    <h1 class="title">{{person$}}</h1>
+    <!-- <h1 class="title">{{person$}}</h1> -->
     <h1 class="title">{{luke$}}</h1>
     <h1 class="title">{{name$}}</h1>
     <img v-stream:error="imageError$" :src="image$" alt>
-    <button class="button" v-stream:click="click$">Click</button>
+    <button class="button" :disabled="disabled$" v-stream:click="click$">{{buttonText$}}</button>
   </section>
 </template>
 
 <script>
 import { interval, of, from, merge } from "rxjs";
-import { map, pluck, switchMap, mapTo } from "rxjs/operators";
+import {
+  map,
+  pluck,
+  // switchMap,
+  mapTo,
+  catchError,
+  share,
+  startWith,
+  exhaustMap
+} from "rxjs/operators";
 
 export default {
   domStreams: ["click$", "imageError$"],
@@ -24,13 +33,28 @@ export default {
     const timesThree$ = interval$.pipe(map(i => i * 3));
     const message$ = of("vue-rx lessons");
     const random$ = this.click$.pipe(map(() => Math.random()));
-    const person$ = from(
-      this.$http.get("https://starwars.egghead.training/people/2")
-    ).pipe(pluck("data"));
-    const luke$ = this.click$.pipe(switchMap(() => person$));
+    const createLoader$ = url => from(this.$http.get(url)).pipe(pluck("data"));
+    const luke$ = this.click$.pipe(
+      mapTo(`https://starwars.egghead.training/people/2`),
+      exhaustMap(createLoader$),
+      catchError(() =>
+        createLoader$("https://starwars.egghead.training/people/1")
+      ),
+      share()
+    );
 
-    const name$ = person$.pipe(pluck("name"));
-    const loadImage$ = person$.pipe(
+    const disabled$ = merge(
+      this.click$.pipe(mapTo(true)),
+      luke$.pipe(mapTo(false))
+    );
+
+    const buttonText$ = disabled$.pipe(
+      startWith(false),
+      map(bool => (bool ? "Loading..." : "Load"))
+    );
+
+    const name$ = luke$.pipe(pluck("name"));
+    const loadImage$ = luke$.pipe(
       map(({ image }) => `https:///starwars.egghead.training/${image}`)
     );
 
@@ -43,10 +67,12 @@ export default {
       timesThree$,
       message$,
       random$,
-      person$,
+      // person$,
       luke$,
       name$,
-      image$
+      image$,
+      disabled$,
+      buttonText$
     };
   }
 };
