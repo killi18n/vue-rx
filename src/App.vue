a50130b4df85d9d571d7c730f7ddc3b3839cf221
Dongho Choi <evals4dead@gmail.com>
2019-02-25 11:15:45 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="section">
+    <h1 class="title">{{message$}}</h1>
+    <h1 class="title">{{timesTwo$}}</h1>
+    <h1 class="title">{{timesThree$}}</h1>
+    <h1 class="title">{{random$}}</h1>
+    <h1 class="title">{{person$}}</h1>
+    <h1 class="title">{{luke$}}</h1>
+    <h1 class="title">{{name$}}</h1>
+    <img v-stream:error="imageError$" :src="image$" alt>
+    <button class="button" v-stream:click="click$">Click</button>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { interval, of, from, merge } from "rxjs";
+import { map, pluck, switchMap, mapTo } from "rxjs/operators";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  domStreams: ["click$", "imageError$"],
+  subscriptions: function() {
+    const interval$ = interval(1000);
+    const timesTwo$ = interval$.pipe(map(i => i * 2));
+    const timesThree$ = interval$.pipe(map(i => i * 3));
+    const message$ = of("vue-rx lessons");
+    const random$ = this.click$.pipe(map(() => Math.random()));
+    const person$ = from(
+      this.$http.get("https://starwars.egghead.training/people/2")
+    ).pipe(pluck("data"));
+    const luke$ = this.click$.pipe(switchMap(() => person$));
+
+    const name$ = person$.pipe(pluck("name"));
+    const loadImage$ = person$.pipe(
+      map(({ image }) => `https:///starwars.egghead.training/${image}`)
+    );
+
+    const failImage$ = this.imageError$.pipe(
+      mapTo(`http://via.placeholder.com/400x400`)
+    );
+    const image$ = merge(loadImage$, failImage$);
+    return {
+      timesTwo$,
+      timesThree$,
+      message$,
+      random$,
+      person$,
+      luke$,
+      name$,
+      image$
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import "buefy/dist/buefy.css";
 
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+
+import Rx from "rxjs";
+import VueRx from "vue-rx";
+
+import Buefy from "buefy";
+
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+Vue.use(VueRx, Rx);
+Vue.use(Buefy);
+Vue.use(VueAxios, axios);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
